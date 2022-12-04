7634b85cf0ed10b8a8c43f627c2380c1b9b8c863
keziacatrece01 <89415386+keziacatrece01@users.noreply.github.com>
2022-12-04 17:30:30 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var BinaryServer = require('binaryjs').BinaryServer;
 var fs = require('fs');
 var wav = require('wav');
+const formidable = require('formidable');
 
 var port = 3700;
 var outFile = 'demo.wav';
@@ -21,27 +22,27 @@ app.listen(port);
 
 console.log('server open on port ' + port);
 
-binaryServer = BinaryServer({port: 9001});
+// binaryServer = BinaryServer({port: 9001});
 
-binaryServer.on('connection', function(client) {
-  console.log('new connection');
+// binaryServer.on('connection', function(client) {
+//   console.log('new connection');
 
-  var fileWriter = new wav.FileWriter(outFile, {
-    channels: 1,
-    sampleRate: 48000,
-    bitDepth: 16
-  });
+//   var fileWriter = new wav.FileWriter(outFile, {
+//     channels: 1,
+//     sampleRate: 48000,
+//     bitDepth: 16
+//   });
 
-  client.on('stream', function(stream, meta) {
-    console.log('new stream');
-    stream.pipe(fileWriter);
+//   client.on('stream', function(stream, meta) {
+//     console.log('new stream');
+//     stream.pipe(fileWriter);
 
-    stream.on('end', function() {
-      fileWriter.end();
-      console.log('wrote to file ' + outFile);
-    });
-  });
-});
+//     stream.on('end', function() {
+//       fileWriter.end();
+//       console.log('wrote to file ' + outFile);
+//     });
+//   });
+// });
 
 
 
@@ -54,6 +55,22 @@ binaryServer.on('connection', function(client) {
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
+});
+
+app.post('/classifier', function(req, res){
+  const form = new formidable.IncomingForm();
+  // Parse `req` and upload all associated files. `files` contains
+  // all files that were uploaded with the form.
+  form.parse(req, function(err, fields, files) {
+    if (err) {
+      return res.status(400).json({ error: err.message });
+    }
+    const [firstFileName] = Object.keys(files);
+
+    //access your ML Model
+
+    res.json({ "genre": xxxx });
+  });
 });
 
 //add the router
